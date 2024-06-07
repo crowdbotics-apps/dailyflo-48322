d694bfc49ed6b3c27798a6967ba135acbca65efb
Crowdbotics <team@crowdbotics.com>
2024-06-07 21:07:09 +0000

    Initial commit

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "dailyflo-48322",
+  url: "https://dailyflo-48322.botics.co",
+  api: "https://dailyflo-48322.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
